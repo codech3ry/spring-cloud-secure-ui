e2dd524b3a20ffde17a2b607422fb233d9274a28
codech3ry <code.ch3ry@gmail.com>
2019-07-28 20:45:58 +1000

    Initial commit

---
+++ src/main/java/com/example/springcloudsecureui/ReportController.java
@@ -0,0 +1,72 @@
+package com.example.springcloudsecureui;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@EnableOAuth2Sso
+public class ReportController extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private OAuth2ClientContext clientContext;
+
+    @Autowired
+    private OAuth2RestTemplate oAuth2RestTemplate;
+
+    @GetMapping("/")
+    public String loadHome() {
+        return "home";
+    }
+
+    @GetMapping("/reports")
+    public String loadReports(Model model) {
+        OAuth2AccessToken t = clientContext.getAccessToken();
+        System.out.println("Token: " + t.getValue());
+
+        ResponseEntity<List<TollUsage>> tolls = oAuth2RestTemplate.exchange("http://localhost:9001/services/tolldata",
+                HttpMethod.GET, null, new ParameterizedTypeReference<List<TollUsage>>() {});
+
+        model.addAttribute("tolls", tolls.getBody());
+        return "reports";
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/login**")
+                .permitAll()
+                .anyRequest().authenticated();
+    }
+
+    public static class TollUsage {
+
+        public String id;
+        public String stationId;
+        public String licensePlate;
+        public String timestamp;
+
+        public TollUsage() {
+        }
+
+        public TollUsage(String id, String stationId, String licensePlate, String timestamp) {
+            this.id = id;
+            this.stationId = stationId;
+            this.licensePlate = licensePlate;
+            this.timestamp = timestamp;
+        }
+    }
+
+}
---
+++ src/main/java/com/example/springcloudsecureui/SpringCloudSecureUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcloudsecureui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudSecureUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudSecureUiApplication.class, args);
+    }
+
+}
